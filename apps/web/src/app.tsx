364c58cbda3e3c22cc57ae2b2d0e257aa9f45068
David <20960328+totallynotdavid@users.noreply.github.com>
2026-04-27 11:02:40 -0500

    remove sentry

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,8 +1,6 @@
-import { withSentryRouterRouting } from "@sentry/solid/solidrouter";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import { isServer } from "solid-js/web";
 
 import { AppErrorBoundary } from "./components/feedback/error/boundary";
 import { ToastContainer } from "./components/feedback/toast/container";
@@ -10,19 +8,13 @@ import { ToastProvider } from "./components/feedback/toast/provider";
 
 import "./app.css";
 
-const SentryRouter = isServer ? Router : withSentryRouterRouting(Router);
-
 export default function App() {
   return (
     <ToastProvider>
       <AppErrorBoundary>
-        <SentryRouter
-          root={(props) => (
-            <Suspense>{props.children}</Suspense>
-          )}
-        >
+        <Router root={(props) => <Suspense>{props.children}</Suspense>}>
           <FileRoutes />
-        </SentryRouter>
+        </Router>
       </AppErrorBoundary>
       <ToastContainer />
     </ToastProvider>
