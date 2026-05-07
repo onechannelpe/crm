9020bf5ecb2fb32ce952be5c906a8acc007a8164
David <20960328+totallynotdavid@users.noreply.github.com>
2026-05-07 03:31:41 -0500

    a

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -3,7 +3,7 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 
 import { AppErrorBoundary } from "./components/feedback/error/boundary";
-import { SnackBarProvider } from "./components/feedback/snack-bar-manager/components/snack-bar-provider";
+import { SnackBarProvider } from "./components/feedback/snack-bar-manager/snack-bar-provider";
 
 import "./app.css";
 
