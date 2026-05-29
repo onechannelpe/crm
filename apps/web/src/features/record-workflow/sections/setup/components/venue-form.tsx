f27576d466989f3ca08eec0a179abefa5ae1fd70
David <20960328+totallynotdavid@users.noreply.github.com>
2026-05-29 02:21:06 -0500

    org

--- apps/web/src/features/record-workflow/sections/setup/components/venue-form.tsx
+++ apps/web/src/features/record-workflow/sections/setup/components/venue-form.tsx
@@ -31,6 +31,35 @@ const MODALIDAD_COBRO_LABELS: Record<ModalidadCobro, string> = {
   CARGO_UNICO: "Cargo único",
 };
 
+function TextFieldRow(props: {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  type?: "text" | "number" | "url";
+  min?: string;
+  step?: string;
+  required?: boolean;
+}) {
+  return (
+    <FieldRow>
+      <FieldLabel>
+        <FieldLabelText>{props.label}</FieldLabelText>
+      </FieldLabel>
+      <FieldInputValue>
+        <TextInput
+          sizeVariant="sm"
+          type={props.type}
+          min={props.min}
+          step={props.step}
+          value={props.value}
+          onChange={props.onChange}
+          required={props.required}
+        />
+      </FieldInputValue>
+    </FieldRow>
+  );
+}
+
 export function VenueForm(props: {
   form: VenueFormState;
   linkScope: ProductScope;
@@ -44,137 +73,84 @@ export function VenueForm(props: {
       <WidgetHeader>
         <WidgetTitle text="Agregar sede" />
       </WidgetHeader>
+
       <WidgetBody>
         <form onSubmit={props.onSubmit}>
           <FieldTable>
-            <FieldRow>
-              <FieldLabel>
-                <FieldLabelText>Nombre comercial</FieldLabelText>
-              </FieldLabel>
-              <FieldInputValue>
-                <TextInput
-                  sizeVariant="sm"
-                  value={props.form.nombreComercial()}
-                  onChange={props.form.setNombreComercial}
-                  required
-                />
-              </FieldInputValue>
-            </FieldRow>
-            <FieldRow>
-              <FieldLabel>
-                <FieldLabelText>Cantidad POS</FieldLabelText>
-              </FieldLabel>
-              <FieldInputValue>
-                <TextInput
-                  sizeVariant="sm"
-                  type="number"
-                  min="1"
-                  step="1"
-                  value={props.form.posQuantity()}
-                  onChange={props.form.setPosQuantity}
-                  required
-                />
-              </FieldInputValue>
-            </FieldRow>
-            <FieldRow>
-              <FieldLabel>
-                <FieldLabelText>Direccion</FieldLabelText>
-              </FieldLabel>
-              <FieldInputValue>
-                <TextInput
-                  sizeVariant="sm"
-                  value={props.form.direccion()}
-                  onChange={props.form.setDireccion}
-                  required
-                />
-              </FieldInputValue>
-            </FieldRow>
-            <FieldRow>
-              <FieldLabel>
-                <FieldLabelText>Referencia</FieldLabelText>
-              </FieldLabel>
-              <FieldInputValue>
-                <TextInput
-                  sizeVariant="sm"
-                  value={props.form.referencia()}
-                  onChange={props.form.setReferencia}
-                  required
-                />
-              </FieldInputValue>
-            </FieldRow>
-            <FieldRow>
-              <FieldLabel>
-                <FieldLabelText>Distrito</FieldLabelText>
-              </FieldLabel>
-              <FieldInputValue>
-                <TextInput
-                  sizeVariant="sm"
-                  value={props.form.distrito()}
-                  onChange={props.form.setDistrito}
-                  required
-                />
-              </FieldInputValue>
-            </FieldRow>
-            <FieldRow>
-              <FieldLabel>
-                <FieldLabelText>Provincia</FieldLabelText>
-              </FieldLabel>
-              <FieldInputValue>
-                <TextInput
-                  sizeVariant="sm"
-                  value={props.form.provincia()}
-                  onChange={props.form.setProvincia}
-                  required
-                />
-              </FieldInputValue>
-            </FieldRow>
-            <FieldRow>
-              <FieldLabel>
-                <FieldLabelText>Departamento</FieldLabelText>
-              </FieldLabel>
-              <FieldInputValue>
-                <TextInput
-                  sizeVariant="sm"
-                  value={props.form.departamento()}
-                  onChange={props.form.setDepartamento}
-                  required
-                />
-              </FieldInputValue>
-            </FieldRow>
+            <TextFieldRow
+              label="Nombre comercial"
+              value={props.form.nombreComercial()}
+              onChange={props.form.setNombreComercial}
+              required
+            />
+
+            <TextFieldRow
+              label="Cantidad POS"
+              type="number"
+              min="1"
+              step="1"
+              value={props.form.posQuantity()}
+              onChange={props.form.setPosQuantity}
+              required
+            />
+
+            <TextFieldRow
+              label="Dirección"
+              value={props.form.direccion()}
+              onChange={props.form.setDireccion}
+              required
+            />
+
+            <TextFieldRow
+              label="Referencia"
+              value={props.form.referencia()}
+              onChange={props.form.setReferencia}
+              required
+            />
+
+            <TextFieldRow
+              label="Distrito"
+              value={props.form.distrito()}
+              onChange={props.form.setDistrito}
+              required
+            />
+
+            <TextFieldRow
+              label="Provincia"
+              value={props.form.provincia()}
+              onChange={props.form.setProvincia}
+              required
+            />
+
+            <TextFieldRow
+              label="Departamento"
+              value={props.form.departamento()}
+              onChange={props.form.setDepartamento}
+              required
+            />
+
             <Show when={props.linkScope === "per_venue"}>
-              <FieldRow>
-                <FieldLabel>
-                  <FieldLabelText>URL Culqi Link</FieldLabelText>
-                </FieldLabel>
-                <FieldInputValue>
-                  <TextInput
-                    sizeVariant="sm"
-                    type="url"
-                    value={props.form.linkUrl()}
-                    onChange={props.form.setLinkUrl}
-                    required
-                  />
-                </FieldInputValue>
-              </FieldRow>
+              <TextFieldRow
+                label="URL Culqi Link"
+                type="url"
+                value={props.form.linkUrl()}
+                onChange={props.form.setLinkUrl}
+                required
+              />
             </Show>
+
             <Show when={props.onlineScope === "per_venue"}>
+              <TextFieldRow
+                label="URL Culqi Online"
+                type="url"
+                value={props.form.onlineUrl()}
+                onChange={props.form.setOnlineUrl}
+                required
+              />
+
               <FieldRow>
                 <FieldLabel>
-                  <FieldLabelText>URL Culqi Online</FieldLabelText>
-                </FieldLabel>
-                <FieldInputValue>
-                  <TextInput
-                    sizeVariant="sm"
-                    type="url"
-                    value={props.form.onlineUrl()}
-                    onChange={props.form.setOnlineUrl}
-                    required
-                  />
-                </FieldInputValue>
-              </FieldRow>
-              <FieldRow>
-                <FieldLabel>
-                  <FieldLabelText>Modalidad de Cobro</FieldLabelText>
+                  <FieldLabelText>Modalidad de cobro</FieldLabelText>
                 </FieldLabel>
                 <FieldInputValue>
                   <div class={styles.radioGroup}>
@@ -203,6 +179,7 @@ export function VenueForm(props: {
           <Show when={props.error}>
             {(message) => <p class={styles.error}>{message()}</p>}
           </Show>
+
           <div class={styles.actions}>
             <Button
               type="submit"
